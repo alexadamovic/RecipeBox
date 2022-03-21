43c73a32beab76bae6f48a2720234f28e2200420
wcjameson <williamjameson90@gmail.com>
2022-03-21 13:18:48 -0700

    add models to application
    Co-authored-by: Alex Adamovic <alexadamovic@users.noreply.github.com>

---
+++ RecipeBox/Models/Rating.cs
@@ -0,0 +1,8 @@
+namespace RecipeBox.Models
+{
+  public class Rating
+  {
+    public int RatingId { get; set; }
+    public int Stars { get; set; }
+  }
+}
---
+++ RecipeBox/Models/RecipeBoxContext.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace RecipeBox.Models
+{
+  public class RecipeBoxContext : IdentityDbContext<ApplicationUser>
+  {
+    public DbSet<Recipe> Recipes { get; set; }
+    public DbSet<Tag> Tags { get; set; }
+    public DbSet<RecipeTag> RecipeTag { get; set; }
+    public DbSet<Rating> Ratings { get; set; }
+
+    public RecipeBoxContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
+  }
+}
